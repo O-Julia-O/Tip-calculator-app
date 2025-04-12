a89a9821e281700f6e8ab58d54c950491f98d4cd
O-Julia-O <julia.qq.onix@gmail.com>
2025-04-12 03:12:02 +0300

    clear all fields after pressing Reset btn

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const tipBtns = document.querySelectorAll(".tip");
+const fieldsAll = document.querySelectorAll("input");
+const resetBtn = document.getElementById("reset-btn");
+
 tipBtns.forEach(button => {
     /* adding listener on every button */
     button.addEventListener("click", () => {
@@ -6,6 +9,9 @@ tipBtns.forEach(button => {
         addingClassName(button, "selected");
     });
 });
+
+resetBtn.addEventListener("click", reset);
+
 function addingClassName(item, className) {
     item.classList.add(className);
 }
@@ -14,5 +20,12 @@ function clearPersentange() {
     tipBtns.forEach(button => {
         button.classList.remove("selected");
     });
+}
+function reset() {
+    fieldsAll.forEach(field => {
+        field.value = "";
+    });
+
+    clearPersentange();
 }
 }
